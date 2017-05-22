60fe3037821761e1b76c06ae886e956c364d177e
Daniel Silvestre <m.c.d.tec@gmail.com>
2017-05-21 21:00:59 -0300

    Update .gitignore

--- modules/rbenv/tests/build.pp
+++
@@ -1,3 +0,0 @@
-class { 'rbenv': }->
-rbenv::plugin { 'rbenv/ruby-build': }->
-rbenv::build { '2.0.0-p247': global => true }
--- modules/rbenv/tests/rbenv-vars.pp
+++
@@ -1,3 +0,0 @@
-package { 'git': ensure => 'installed' }->
-class { 'rbenv': }->
-rbenv::plugin { 'rbenv/rbenv-vars': }
